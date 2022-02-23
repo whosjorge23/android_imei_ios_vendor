94f9ad7595b495d36e107e72a944f99a3a8747c3
jorge_upa@msn.com <jorge_upa@msn.com>
2022-02-23 14:14:49 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/android_imei_ios_vendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.android_imei_ios_vendor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runZonedGuarded(() {
+    runApp(const MyApp());
+  }, (dynamic error, dynamic stack) {
+    developer.log("Something went wrong!", error: error, stackTrace: stack);
+  });
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
+  final Map<String, String> _user = {
+    "username": "",
+    "deviceIdentifier": "",
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    var deviceData = <String, dynamic>{};
+
+    try {
+      if (Platform.isAndroid) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+
+    var user1 = {
+      "username": "Giorgio",
+      "deviceIdentifier":
+          "${_deviceData[Platform.isAndroid ? "androidId" : "identifierForVendor"]}"
+    };
+    _user.addEntries(user1.entries);
+    print(_user);
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      // 'version.securityPatch': build.version.securityPatch,
+      // 'version.sdkInt': build.version.sdkInt,
+      // 'version.release': build.version.release,
+      // 'version.previewSdkInt': build.version.previewSdkInt,
+      // 'version.incremental': build.version.incremental,
+      // 'version.codename': build.version.codename,
+      // 'version.baseOS': build.version.baseOS,
+      // 'board': build.board,
+      // 'bootloader': build.bootloader,
+      // 'brand': build.brand,
+      // 'device': build.device,
+      // 'display': build.display,
+      // 'fingerprint': build.fingerprint,
+      // 'hardware': build.hardware,
+      // 'host': build.host,
+      // 'id': build.id,
+      // 'manufacturer': build.manufacturer,
+      // 'model': build.model,
+      // 'product': build.product,
+      // 'supported32BitAbis': build.supported32BitAbis,
+      // 'supported64BitAbis': build.supported64BitAbis,
+      // 'supportedAbis': build.supportedAbis,
+      // 'tags': build.tags,
+      // 'type': build.type,
+      // 'isPhysicalDevice': build.isPhysicalDevice,
+      'androidId': build.androidId,
+      // 'systemFeatures': build.systemFeatures,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      // 'name': data.name,
+      // 'systemName': data.systemName,
+      // 'systemVersion': data.systemVersion,
+      // 'model': data.model,
+      // 'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      // 'isPhysicalDevice': data.isPhysicalDevice,
+      // 'utsname.sysname:': data.utsname.sysname,
+      // 'utsname.nodename:': data.utsname.nodename,
+      // 'utsname.release:': data.utsname.release,
+      // 'utsname.version:': data.utsname.version,
+      // 'utsname.machine:': data.utsname.machine,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            Platform.isAndroid
+                ? 'Android Device Info'
+                : Platform.isIOS
+                    ? 'iOS Device Info'
+                    : '',
+          ),
+        ),
+        body: ListView(
+          children: _deviceData.keys.map(
+            (String property) {
+              return Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      property,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                      child: Text(
+                        '${_deviceData[property]}',
+                        maxLines: 10,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
